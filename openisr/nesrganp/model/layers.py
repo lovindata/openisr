d6a599e4d06d170c3bd4da194650fbbdb204b940
iLoveDataJjia <ilovedata.jjia@gmail.com>
2021-08-13 08:33:13 +0200

    WIP on layers.py

---
+++ openisr/nesrganp/model/layers.py
@@ -0,0 +1,22 @@
+
+import torch
+import torch.nn as nn
+from torch.onnx import TrainingMode
+
+class GaussianNoise(nn.Module):
+    def __init__(self):
+        super(GaussianNoise, self).__init__()
+        self.stddev = 0.1
+
+    def forward(self, x):
+        if self.training:
+            x_scale = self.stddev * x
+            x = torch.normal(mean=x, std=x_scale)
+        return x
+
+class ResidualDenseBlock(nn.Module):
+
+    def __init__(self) -> None:
+        super(ResidualDenseBlock, self).__init__()
+
+        self.noise = GaussianNoise()
