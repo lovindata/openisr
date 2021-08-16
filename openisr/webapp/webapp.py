2676964c31753476fe5dc9c6c69998a55e37ec2c
iLoveDataJjia <ilovedata.jjia@gmail.com>
2021-08-16 21:02:54 +0200

    ✨ Dev webapp (WIP on the base page)

---
+++ openisr/webapp/webapp.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+#from models import MobileNet
+import os
+from math import floor
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+#model = MobileNet()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/infer', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        saveLocation = f.filename
+        f.save(saveLocation)
+        inference, confidence = None #model.infer(saveLocation)
+        # make a percentage with 2 decimal points
+        confidence = floor(confidence * 10000) / 100
+        # delete file after making an inference
+        os.remove(saveLocation)
+        # respond with the inference
+        return render_template('inference.html', name=inference, confidence=confidence)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port, debug=True)
