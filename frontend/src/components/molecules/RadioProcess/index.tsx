c1a32a115fb66ad8155bd40a8b4695c3aa27c40f
iLoveDataJjia <ilovedata.jjia@gmail.com>
2024-01-24 07:50:13 +0100

    WIP: Endpoint issue for "src" + Continue frontend dev images section/card

---
+++ frontend/src/components/molecules/RadioProcess/index.tsx
@@ -0,0 +1,34 @@
+import { BorderBox } from "../../atoms/BorderBox";
+import { ProcessOptions } from "./ProcessOptions";
+import { RadioGroup } from "@headlessui/react";
+
+interface Props {
+  value: ProcessOptions;
+  setValue: (value: ProcessOptions) => void;
+}
+
+export function RadioProcess({ value, setValue }: Props) {
+  const values = Object.values(ProcessOptions);
+  return (
+    <RadioGroup value={value} onChange={setValue}>
+      <BorderBox className="grid h-8 w-72 grid-flow-col justify-stretch divide-x text-xs">
+        {Object.values(ProcessOptions).map((option, idx) => (
+          <RadioGroup.Option value={option} key={idx}>
+            {({ checked }) => (
+              <label
+                className={
+                  "flex h-full cursor-pointer items-center justify-center" +
+                  (checked ? " bg-white text-slate-950" : "") +
+                  (idx == 0 ? " rounded-l-lg" : "") +
+                  (idx == values.length - 1 ? " rounded-r-lg" : "")
+                }
+              >
+                {option}
+              </label>
+            )}
+          </RadioGroup.Option>
+        ))}
+      </BorderBox>
+    </RadioGroup>
+  );
+}
--- frontend/src/components/organisms/ImagesSection.tsx
+++ frontend/src/components/organisms/ImagesSection.tsx
@@ -1,10 +1,24 @@
 import { SectionHeader } from "../atoms/SectionHeader";
+import { ImageCard } from "../molecules/ImageCard";
+import { RadioProcess } from "../molecules/RadioProcess";
+import { ProcessOptions } from "../molecules/RadioProcess/ProcessOptions";
 import { SearchBar } from "../molecules/SearchBar";
-import { TabBar } from "../molecules/TabBar";
 import { useState } from "react";
 
-export function ImagesSection() {
+interface Props {
+  images:
+    | {
+        id: number;
+        src: string;
+        name: string;
+        source: { width: number; height: number };
+      }[]
+    | undefined;
+}
+
+export function ImagesSection({ images }: Props) {
   const [search, setSearch] = useState("");
+  const [option, setOption] = useState<ProcessOptions>(ProcessOptions.All);
 
   return (
     <div className="md:space-y-3">
@@ -14,7 +28,18 @@ export function ImagesSection() {
           value={search}
           onChange={(event) => setSearch(event.target.value)}
         />
-        <TabBar />
+        <RadioProcess value={option} setValue={setOption} />
+      </div>
+      <div className="grid grid-cols-2 gap-3">
+        {images?.map((image) => (
+          <ImageCard
+            key={image.id}
+            src={image.src}
+            name={image.name}
+            width={image.source.width}
+            height={image.source.height}
+          />
+        ))}
       </div>
     </div>
   );
